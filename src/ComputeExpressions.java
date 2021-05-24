e2d69aafb87ab59fb4ea0981b66dc375b92f157a
selden <zac_selden@pm.me>
2021-05-24 11:21:05 -0500

    <init ComputeExpressions>

---
+++ src/ComputeExpressions.java
@@ -0,0 +1,12 @@
+/*
+Selden
+May 24, 2021
+Description: Compute expressions
+ */
+
+public class ComputeExpressions {
+    public static void main(String[] args) {
+        System.out.print("(10.5 + 2 * 3) / (45 - 3.5) = ");
+        System.out.print((10.5 + 2 * 3) / (45 - 3.5));
+    }
+}
---
+++ src/Template.java
@@ -0,0 +1,8 @@
+/*
+Selden
+May 24, 2021
+Description: Rewrite the first program to display three messages.
+ */
+
+public class Template {
+}
