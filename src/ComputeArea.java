6ac3294d945a32b6d876b2328f1e2755aa14429f
selden <zac_selden@pm.me>
2021-05-24 14:55:53 -0500

    <init ComputeAreaWithConsoleInput>

---
+++ src/ComputeArea.java
@@ -0,0 +1,15 @@
+public class ComputeArea {
+    public static void main(String[] args) {
+
+        // declare
+        double radius;
+        double area;
+
+        // assign
+        radius = 20;
+        area = radius * radius * 3.14159;
+
+        // display results
+        System.out.println("The area for the circle of radius " + radius + " is " + area);
+    }
+}
