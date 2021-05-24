90fa164bb3f064b7005d8efb0d8c0ec025377f63
selden <zac_selden@pm.me>
2021-05-24 11:09:50 -0500

    <init welcome>

---
+++ src/Welcome.java
@@ -0,0 +1,12 @@
+/*
+Selden
+May 24, 2021
+Description: Display the following message: Welcome to Java!
+ */
+
+public class Welcome {
+    public static void main(String[] args) {
+        // display message Welcome to Java! on console
+        System.out.println("Welcome to Java!");
+    }
+}
