083a236609db0d1c74d5bc23e55ba629882d0610
selden <zac_selden@pm.me>
2021-05-24 11:13:20 -0500

    <init WelcomeWithThreeMessages>

---
+++ src/WelcomeWithThreeMessages.java
@@ -0,0 +1,13 @@
+/*
+Selden
+May 24, 2021
+Description: Rewrite the first program to display three messages.
+ */
+
+public class WelcomeWithThreeMessages {
+    public static void main(String[] args) {
+        System.out.println("Programming is fun!");
+        System.out.println("Fundamentals First");
+        System.out.println("Problem Driven");
+    }
+}
